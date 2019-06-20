53f11fb90e09ca13fc07d9bda395e9dbb49be6fc
mongare254 <mongarew3@gmail.com>
2019-06-20 14:55:47 +0300

    first commit

---
+++ hospital/test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Connecting MySQL Server</title>
+</head>
+<body>
+<?php
+   $name= William;
+   print("Welcome, $name");
+   $many = 2.2888800;
+$many_2 = 2.2111200;
+$few = $many + $many_2;
+print($many + $many_2 = $few );
+?>
+</body>
+</html>
